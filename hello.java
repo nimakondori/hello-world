feec4b8f1a15d614a00f5060753e6b00c2a4c6e9
nimakondori96 <nimakondori96@gmail.com>
2019-07-18 13:46:07 -0700

    java added

---
+++ hello.java
@@ -0,0 +1,12 @@
+import murmur.resolution.nima.BubbleService;
+
+
+private synchronized void helloService {
+    private English Hello = "Hello";
+    private Spanish Hello = "Hola";
+    private Farsi   Hello = "Salam";
+    private Turkish Hello = "Merhaba";
+    private Arabic  Hello = "Asalamo Alaikom";
+    // Needs to be double checked
+    System.out.println("Hello Sir");    
+}
